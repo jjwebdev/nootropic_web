409c903eb6b3a3f2772b675bd49e962c25c448b4
John Nguyen <jnguyen@thefrontiergroup.com.au>
2016-10-12 00:08:01 +0800

    fix cp

--- config/travis_ci_test.exs
+++ config/travis_ci_test.exs
@@ -1,7 +1,10 @@
 use Mix.Config
-
-# Configure your database
-config :agile_pulse, AgilePulse.Repo,
+config :noot, Noot.Endpoint,
+  http: [port: 4001],
+  server: false
+config :logger, level: :warn
+config :comeonin, bcrypt_log_rounds: 4
+config :noot, Noot.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
